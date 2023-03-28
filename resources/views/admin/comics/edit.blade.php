4f9e8581ddba46290700375b61ce362e10c24601
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-03-28 17:21:08 +0200

    Aggiornamenti

--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -11,21 +11,21 @@
         </div>
         <div class="row">
             <div class="col">
-                <form action="{{ route('admin.posts.update', $post) }}" method="POST">
+                <form action="{{ route('admin.posts.update', $post) }}" method="POST" enctype="multipart/form-data">
 
                     @csrf
                     @method('PUT')
 
                     <div>
-                        <label for="name" class="form-label">Nome Fumetto</label>
+                        <label for="name" class="form-label">Nome Fumetto*</label>
                         <input type="text" name="name" id="name" class="form-control" required maxlength="128" value="{{ old('name', $comic->name) }}" placeholder="Inserisci Nome del Progetto">
                     </div>
                     <div>
-                        <label for="price" class="form-label">Super Prezzo</label>
+                        <label for="price" class="form-label">Super Prezzo*</label>
                         <input type="number" name="price" id="price" class="form-control" value="{{ old('price', $comic->price) }}" required placeholder="Inserisci il prezzo del fumetto">
                     </div>
                     <div>
-                        <label for="quantity" class="form-label">Quantità</label>
+                        <label for="quantity" class="form-label">Quantità*</label>
                         <input type="number" name="quantity" id="quantity" class="form-control" value="{{ old('quantity', $comic->quantity) }}" required placeholder="Inserisci la quantità">
                     </div>
                     <div>
@@ -35,6 +35,11 @@
                     <div>
                         <label for="image" class="form-label">Immagine</label>
                         <input type="file" name="image" id="image" class="form-control" value="{{ old('url', $comic->url) }}">
+                    </div>
+                    <div class="mb-3">
+                        <p>
+                            The fields with * are <strong>compulsory</strong>.
+                        </p>
                     </div>
                     <div>
                         <button type="submit" class="btn btn-success mt-5">
